51fdf6327e7e30e63bdfe289b1dc6c1ed3696b08
hiroya.shimamoto <shshimamo@gmail.com>
2021-10-09 04:19:10 +0900

    (service) update bin/service.ts

--- infra/service/bin/service.ts
+++ infra/service/bin/service.ts
@@ -4,18 +4,33 @@ import * as cdk from '@aws-cdk/core';
 import { ServiceStack } from '../lib/service-stack';
 
 const app = new cdk.App();
+
+const stage: string = app.node.tryGetContext('stage')
+if (!stage.match(/^(stg|prd)$/)) {
+  console.warn('Invalid context. stage must be stg or prd.')
+  process.exit(1)
+}
+let tagOrDigest: string | undefined
+const tag: string = app.node.tryGetContext('tag')
+if (tag) {
+  tagOrDigest = `:${tag}`
+}
+const digest: string = app.node.tryGetContext('digest')
+if (!tag && digest) {
+  tagOrDigest = `@${digest}`
+}
+if (!tagOrDigest) {
+  console.warn('Invalid context. tag or digest must be required.')
+  process.exit(1)
+}
+
+const env = app.node.tryGetContext(stage)
+console.log({ stage, env, tagOrDigest })
+
 new ServiceStack(app, 'ServiceStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '', region: '' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+  stackName: 'ServiceStack',
+  env,
+  cpu: 512,
+  memory: 1024,
+  tagOrDigest
+})
