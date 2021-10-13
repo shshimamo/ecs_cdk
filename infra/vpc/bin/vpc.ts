93c30a70a604d200f6b70a05609f57a71b00dcd1
hiroya.shimamoto <shshimamo@gmail.com>
2021-10-14 03:18:05 +0900

    first deploy
    とりあえずデプロイ完了
    
    ・frontendとbackendの繋ぎ込みはこれから
    ・backendはとりあえずdevelopment,sqlite

--- infra/vpc/bin/vpc.ts
+++ infra/vpc/bin/vpc.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { VpcStack } from '../lib/vpc-stack';
 
 const app = new cdk.App();
-new VpcStack(app, 'VpcStack', {
+new VpcStack(app, 'EcsCdkVpcStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
